54719d434197ddf2dccfd35f1bb26556525275d7
Bertrand <bertrand.tang.dev@gmail.com>
2025-08-27 11:36:04 +0200

    ajout du reste  du code des tests

--- store.js
+++ store.js
@@ -2,7 +2,7 @@ import fs from "node:fs/promises";
 import path from "node:path";
 
 const filename = path.join(process.cwd(), "db.json");
-const saveChanges = (data) => fs.writeFile(filename, JSONstringify(data));
+const saveChanges = (data) => fs.writeFile(filename, JSON.stringify(data));
 const readData = async () => {
     const data = await fs.readFile(filename, "utf-8");
     return JSON.parse(data);
@@ -17,13 +17,13 @@ const create = async (message) => {
     const newItem = { message, id: data.length + 1 };
     await saveChanges(data.concat([newItem]));
     return newItem;
-}
+};
 const updateById = async (id, message) => {
     const data = await readData();
     const newData = data.map((current) => {
         if (current.id === id) {
             return { ...current, message };
-        };
+        }
         return current;
     });
     await saveChanges(newData);
@@ -32,4 +32,4 @@ const deleteById = async (id) => {
     const data = await readData();
     await saveChanges(data.filter((current) => current.id !== id));
 };
-export { getAll, getById, create, updateById, deleteById }
+export { getAll, getById, create, updateById, deleteById };
